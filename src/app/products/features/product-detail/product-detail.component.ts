0a6c5ec94fd4f3cfa1a8bc41e9c5d33ef14bbd20
Gabriel Vejar <g.vejarperez@gmail.com>
2024-09-13 21:23:44 -0300

    add product to cart from product details

--- src/app/products/features/product-detail/product-detail.component.ts
+++ src/app/products/features/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, effect, inject, input } from '@angular/core';
 import { ProductDetailStateService } from '../../data-access/product-detail-state.service';
 import { CurrencyPipe, TitleCasePipe } from '@angular/common';
+import { CartStateService } from '../../../shared/data-access/cart-state.service';
 
 // params metodo 1
 // import { Component, inject } from '@angular/core';
@@ -16,6 +17,7 @@ import { CurrencyPipe, TitleCasePipe } from '@angular/common';
 })
 export default class ProductDetailComponent {
   productDetailState = inject(ProductDetailStateService).state;
+  cartState = inject(CartStateService).state;
 
   // params metodo 1
   // private activatedRoute = inject(ActivatedRoute);
@@ -30,8 +32,13 @@ export default class ProductDetailComponent {
 
   constructor() {
     effect(() => {
-      console.log(this.id());
       this.productDetailState.getById(this.id());
+    });
+  }
+  addToCart() {
+    this.cartState.add({
+      product: this.productDetailState().product!,
+      quantity: 1,
     });
   }
 }
